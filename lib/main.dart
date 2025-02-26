02802c043f7f356c2dde09590fe70d0771fb20c2
SoumirimaGhorai <soumirimaghorai@gmail.com>
2025-02-26 13:55:17 +0530

    j

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '1st project',
       theme: ThemeData(
         appBarTheme: AppBarTheme(backgroundColor: Colors.transparent),
